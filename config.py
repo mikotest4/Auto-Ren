402103c7c97aac1e45a81b552e92dc7b77fafc1d
mikotest4 <fakealok756@gmail.com>
2025-06-24 15:10:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,22 +5,22 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")
-    API_HASH  = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "736532225-w") 
+    API_ID    = os.environ.get("API_ID", "27704224")
+    API_HASH  = os.environ.get("API_HASH", "3q3ij.mongodb.net/?retryWrites=true&w=majority")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7540338860:AAFSgM-h827cvQg0E8XQ-I--W-kjPqfKgyg") 
 
     # database config
     DB_NAME = os.environ.get("DB_NAME","Yato")     
-    DB_URL  = os.environ.get("DB_URL","mongodb")
-    PORT = os.environ.get("PORT", "8080")
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://ZeroTwo:aloksingh@zerotwo.3q3ij.mongodb.net/?retryWrites=true&w=majority")
+    PORT = os.environ.get("PORT", "1120")
  
     # other configs
     BOT_UPTIME  = time.time()
-    START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/29a3acbbab9de5f45a5fe.jpg")
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6497757690').split()]
+    START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/51332f55a49e56fc28ee2-6ff539b07e6be4f749.jpg")
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '7970350353').split()]
     FORCE_SUB_CHANNELS = os.environ.get('FORCE_SUB_CHANNELS', 'codeflix_bots').split(',')
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001868871195"))
-    DUMP_CHANNEL = int(os.environ.get("DUMP_CHANNEL", "-1001868871195"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002669902570"))
+    DUMP_CHANNEL = int(os.environ.get("DUMP_CHANNEL", "-1002669902570"))
     
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
